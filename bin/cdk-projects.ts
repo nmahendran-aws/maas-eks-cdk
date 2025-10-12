5e48db9cddd487e206561e7712d7b8c19b69f515
nmahendran-aws <75538437+nmahendran-aws@users.noreply.github.com>
2025-10-12 16:18:06 -0400

    Update cdk-projects.ts

--- bin/cdk-projects.ts
+++ bin/cdk-projects.ts
@@ -5,7 +5,7 @@ import { CdkProjectsStack } from '../lib/cdk-projects-stack';
 
 const app = new cdk.App();
 new CdkProjectsStack(app, 'CdkProjectsStack', {
-  env: { account: '031225117655', region: 'us-east-1' },
+  env: { account: process.env.ACC_ID, region: 'us-east-1' },
   // Provide your VPC ID here or via environment variable VPC_ID
   vpcId: process.env.VPC_ID,
 });
